1f80fdfa4d8eb21c92df3c4273a24c5c18c1e700
Hossein Shafiei <h.shafiei@ut.ac.ir>
2019-04-15 17:29:18 +0430

    1

--- lib/mailservice.rb
+++ lib/mailservice.rb
@@ -2,6 +2,8 @@ module Mailservice
   require 'net/http'
   require 'uri'
   require 'json'
+  require 'openssl'
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
   def send_mail(to, subject , text, url)
     conn = Faraday.new(:url => 'https://sn.ut.ac.ir:4200')
--- lib/webpush.rb
+++ lib/webpush.rb
@@ -2,6 +2,8 @@ module Webpush
   require 'net/http'
   require 'uri'
   require 'json'
+  require 'openssl'
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
   def send_web(id, title, body)
     conn = Faraday.new(:url => 'https://sn.ut.ac.ir:4200')
